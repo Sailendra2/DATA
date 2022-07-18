ceae8deff65ca0e75a5c346ced7791db77de9335
003RSV744 <003RSV744@host.docker.internal>
2022-07-18 11:52:15 +0530

    hello

---
+++ hello-boot/src/main/java/com/ibm/rest/LoginController.java
@@ -0,0 +1,18 @@
+package com.ibm.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+	@GetMapping(value = "/login", produces = "text/html")// http://localhost:8880/login?username=scott&password=tiger
+	public String validate(@RequestParam String username, @RequestParam String password) {
+		if(username.equals("scott") && password.equals("tiger"))
+			return "<h1>Login Successful</h1>";
+		else
+			return "<h2>Login Failed</h2>";
+	}
+
+}
