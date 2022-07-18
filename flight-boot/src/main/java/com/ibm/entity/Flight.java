50017ff7d81bcd2647e9562b9b67d2f691c9100a
003RSV744 <003RSV744@host.docker.internal>
2022-07-18 11:58:15 +0530

    hii

---
+++ flight-boot/src/main/java/com/ibm/entity/Flight.java
@@ -0,0 +1,48 @@
+package com.ibm.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "flights")
+public class Flight {
+
+	@Id
+	private int code;
+	@Column(length = 15)
+	private String carrier;
+	@Column(length = 15)
+	private String source;
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getCarrier() {
+		return carrier;
+	}
+	public void setCarrier(String carrier) {
+		this.carrier = carrier;
+	}
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	public String getDestination() {
+		return destination;
+	}
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+	@Column(length = 15)
+
+	private String destination;
+
+	
+}
